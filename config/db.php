1331a09f8d26bdf5c8294c76132f9e010c7c25ef
Milivoje <milivojeivic422@gmail.com>
2017-05-22 02:18:07 +0200

    DB config changed for local server

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=sql204.byethost11.com;dbname=b11_17818273_ynews',
-    'username' => 'b11_17818273',
-    'password' => '57815782Ivica',
+    'dsn' => 'mysql:host=localhost;dbname=bravoarcade',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
